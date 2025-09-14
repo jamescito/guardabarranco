f0e28d5cd66b2b8c8629104e40f9ec37c0e52851
james <jr76407900@gmail.com>
2025-09-14 16:36:06 -0600

    Creamos controller y model de Operador local

---
+++ app/Http/Controllers/OperadorLocalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OperadorLocal;
+use Illuminate\Http\Request;
+
+class OperadorLocalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OperadorLocal $operadorLocal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OperadorLocal $operadorLocal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OperadorLocal $operadorLocal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OperadorLocal $operadorLocal)
+    {
+        //
+    }
+}
